2da41665c56b1915de12231b0659d6b9d2ce7db1
sebasp-125 <sp6027193@gmail.com>
2023-10-13 17:11:50 -0500

    Ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,5 @@
+//jercicio #3
+let grados = prompt("Ingrese los grados Centigrados: ");
+let resultado = (grados * 1.8) + 32;
+
+alert( `Los grados Centigrados fueron: ${grados} y los grados Fahrenheit son: ${resultado}`);
