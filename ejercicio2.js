64a9deeb745723d401472d2fbcc92b7a528136b9
Juan Chaverra <juaneschaverra4822@gmail.com>
2023-10-13 16:47:19 -0500

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,5 @@
+//Hacer un programa que convierta los grados centígrados (30 grados) a grados Fahrenheit, la fórmula es la siguiente: (C*1.8) +32. Imprimir los resultados en una alerta.
+
+let grados = 30;
+let resultado = (grados * 1.8) + 32;
+alert( `Los grados Centigrados fueron: ${grados} y los grados Fahrenheit son: ${resultado}`);
